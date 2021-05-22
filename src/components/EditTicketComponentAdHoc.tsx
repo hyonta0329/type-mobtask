13686597e2c7ee4dcbe9e9a541bbd03dfda22b81
Takahashi Hiroaki <hiroakta@jp.co.square-enix.com>
2021-05-23 08:28:35 +0900

    レイアウト微修正

--- src/components/EditTicketComponentAdHoc.tsx
+++ src/components/EditTicketComponentAdHoc.tsx
@@ -141,14 +141,14 @@ class Component extends React.Component<Props, State>{
                         </tr>
                         <tr>
                             <td id="edithead"> 
-                                <input type="text"　size={30} value={this.state.title}  id="title" onChange={this.handleChangeTITLE.bind(this)} /> 
+                                <input type="text"　size={10} value={this.state.title}  id="title" onChange={this.handleChangeTITLE.bind(this)} /> 
                             </td>
                         </tr>
                         <tr>
                                 <td id="edithead">Comment: </td></tr>
                         <tr>
                             <td>
-                                <textarea id="textarea" value={this.state.Comment} cols={30} rows={17} onChange={this.handleChangeCOMMENTtextArea.bind(this)}/>
+                                <textarea id="textarea" value={this.state.Comment} cols={10} rows={17} onChange={this.handleChangeCOMMENTtextArea.bind(this)}/>
                             </td>
                         </tr>
                         <tr>
